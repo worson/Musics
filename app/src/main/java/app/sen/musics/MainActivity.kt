bb61610e598c05a1a58b34c3e20a5ff1c5ac1d36
wangshengxing <www.sheng.xing@gmail.com>
2020-08-20 21:25:48 +0800

    nothing

--- app/src/main/java/app/sen/musics/MainActivity.kt
+++ app/src/main/java/app/sen/musics/MainActivity.kt
@@ -19,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_musics, R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        navController.navigate(R.id.navigation_dashboard)
+        navController.navigate(R.id.navigation_musics)
     }
 }
